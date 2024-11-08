c5c17629a6d28ec815364672bec40fd5aafd5cc1
Ivan Petrischev <petrischev.vanya2002@yandex.ru>
2024-11-08 14:26:30 +0300

    update project

--- app/views.py
+++ app/views.py
@@ -1,18 +1,38 @@
+import copy
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 QUESTIONS = [
     {
         'title': f'title {i}',
         'id': i,
         'text': f'This is text for question # {i}'
-    } for i in range(1, 30)
+    } for i in range(30)
 ]
 
 
 def index(request):
+    page_num = int(request.GET.get('page', 1))
+    paginator = Paginator(QUESTIONS, 5)
+    page = paginator.page(page_num)
     return render(
         request, 
         'index.html', 
-        context={'questions':QUESTIONS}
+        context={'questions':page.object_list, 'page_obj':page}
     )
+
+
+def hot(request):
+    hoy_questions = copy.deepcopy(QUESTIONS)
+    hoy_questions.reverse()
+    return render(
+        request, 
+        'hot.html', 
+        context={'questions':hoy_questions}
+    )
+
+def question(request, question_id):
+    one_question = QUESTIONS[question_id]
+    return render(request, 'one_question.html', 
+                {'item': one_question})
--- askme_petrischev_py/urls.py
+++ askme_petrischev_py/urls.py
@@ -21,5 +21,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'), 
+    path('question/<int:question_id>', views.question, name='one_question'), 
     path('admin/', admin.site.urls),
 ]
